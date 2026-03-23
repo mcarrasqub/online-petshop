63fdc39304c000f9f42c65e3c7961dcb1cb5cdd3
mariana <mcarrasquilla323@gmail.com>
2026-03-23 16:00:26 -0500

    Refactor ExampleTest to improve redirection tests for user roles

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,35 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    
+    use RefreshDatabase;
+
+    /** @test */
+    public function redirect_to_login_when_guest_visits_home()
     {
         $response = $this->get('/');
+        $response->assertRedirect(route('login'));
+    }
 
-        $response->assertStatus(200);
+    /** @test */
+    public function redirect_to_admin_home_when_admin_visits_home()
+    {
+        $admin = User::factory()->create(['is_admin' => true]);
+        $response = $this->actingAs($admin)->get('/');
+        $response->assertRedirect(route('admin.home.index'));
+    }
+
+    /** @test */
+    public function redirect_to_product_index_when_user_visits_home()
+    {
+        $user = User::factory()->create(['is_admin' => false]);
+        $response = $this->actingAs($user)->get('/');
+        $response->assertRedirect(route('product.index'));
     }
 }
